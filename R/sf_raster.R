c7ed6a22ebf6a69f5c00a4b3729f219576f8cb88
Seongeun Jeong <jeongse@gmail.com>
2020-07-01 21:35:14 -0700

    added convert_df_to_sf_obj

--- R/sf_raster.R
+++ R/sf_raster.R
@@ -12,3 +12,21 @@ get_crs_from_sf_obj <- function (sf_obj) {
   crs_str = st_crs (sf_obj)
   return (crs_str)
 }
+
+
+#' Convert data frame to sf object
+#'
+#' @param df data frame with lon and lat
+#' @param crs_num_or_str projection string or the corresponding EPSG number
+#'
+#' @return sf object.
+#' @export
+#'
+#' @examples convert_df_to_sf_obj (df, 4326)
+#'
+convert_df_to_sf_obj <- function (df, crs_num_or_str = 4326) {
+  assertthat::assert_that('lon' %in% names (df))
+  assertthat::assert_that('lat' %in% names (df))
+  df_sf = df %>% st_as_sf(coords = c("lon", "lat"), crs = crs_num_or_str)
+  return (df_sf)
+}

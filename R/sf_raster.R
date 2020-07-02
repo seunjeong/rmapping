3ac71b866355df64da64183474355ed8689429db
Seongeun Jeong <jeongse@gmail.com>
2020-07-01 20:50:41 -0700

    adding get_crs_from_sfc_obj

---
+++ R/sf_raster.R
@@ -0,0 +1,12 @@
+#' Get projection info from a sf object.
+#'
+#' @param sf_obj object to extract projection info from.
+#'
+#' @return projection info.
+#' @export
+#'
+#' @examples get_crs_from_sf_obj (sf_obj)
+get_crs_from_sf_obj <- function (sf_obj) {
+  crs_str = st_crs (sf_obj)
+  return (crs_str)
+}

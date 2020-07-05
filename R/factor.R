2f53ab0ac92d1751e5c77b6704ded9d03af65584
Seongeun Jeong <jeongse@gmail.com>
2020-07-04 18:02:26 -0700

    fix factor.R

--- R/factor.R
+++ R/factor.R
@@ -7,8 +7,7 @@ require (plyr)
 #' @return renamed factors
 #' @export
 #'
-#' @examples
-#' rename_factor (x, vector_map)
+#' @examples rename_factor (x, vector_map)
 #' vector_map = c('< 3.0' = '727', '3.0 to 3.9' = '702', '4.0 to 4.9' = '612',
 #   '5.0 to 5.9' = '519', '6.0 to 6.9' = '443', '7.0 to 7.9' = '366',
 #   '8.0 to 8.9' = '296', '9.0 - 9.9' = '237', '\u2265 10' = '125')

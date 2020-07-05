befddade5bfb2c2819314536b5cf0263788d212a
Seongeun Jeong <jeongse@gmail.com>
2020-07-04 20:02:41 -0700

    fix error

--- R/io_spatial.R
+++ R/io_spatial.R
@@ -2,12 +2,13 @@ require(ncdf4)
 
 #' Get a variable from netcdf file or ncdf4 object.
 #'
-#' @param ncin
-#' @param var_name
+#' @param ncin string or ncdf4 object
+#' @param var_name variable name of type string.
 #'
 #' @return
 #' @export
 #' @examples get_ncdf_var (ncin, var_name)
+
 get_ncdf_var <- function (ncin, var_name) {
   if (class(ncin) != "ncdf4") {
     ncin <- nc_open(ncin)

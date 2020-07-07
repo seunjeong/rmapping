4cd29ca0a27b4c1627e070f962ba20be660b73ed
Seongeun Jeong <jeongse@gmail.com>
2020-07-07 16:46:13 -0700

    add import to base_map.R

--- R/base_map.R
+++ R/base_map.R
@@ -1,15 +1,13 @@
-require(USAboundaries)
-require(USAboundariesData)
-require(dplyr)
-require(sf)
-
 #' Get US Zip Code boundaries in sf object
 #'
 #' @param epsg_num
 #'
 #' @return
 #' @export get_us_zipcode
-#'
+#' @import USAboundaries
+#' @import dplyr
+#' @import sf
+#' @import magrittr
 #' @examples
 get_us_zipcode <- function (epsg_num = 4326) {
   zip = USAboundaries::us_zipcodes() %>% sf::st_transform(crs=epsg_num)

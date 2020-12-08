2fff80be42e06dbdffb5c0a8d922cd4802fc7356
Seongeun Jeong <jeongse@gmail.com>
2020-12-07 16:59:01 -0800

    get_conus_map

--- R/base_map.R
+++ R/base_map.R
@@ -13,3 +13,21 @@ get_us_zipcode <- function (epsg_num = 4326) {
   zip = USAboundaries::us_zipcodes() %>% sf::st_transform(crs=epsg_num)
   return (zip)
 }
+
+#' Get CONUS base map
+#'
+#' @param epsg_num integer for EPSG code
+#'
+#' @return
+#' @export get_conus_map
+#' @import sf
+#' @import USAboundaries
+#' @import dplyr
+#' @examples
+get_conus_map <- function (epsg_num = 4326) {
+  us_state_high_res = USAboundaries::us_states(res = 'high') %>%
+                      sf::st_transform(crs = epsg_num)
+  us_conus_high_res = us_state_high_res %>%
+      dplyr::filter (!state_abbr %in% c('AK', 'HI', 'PR', 'GU', 'MP', 'VI', 'AS'))
+  return (us_conus_high_res)
+}
